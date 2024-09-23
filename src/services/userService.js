b59185f8e5d85f2862b75822d22ee2dd80ee603f
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-09-22 23:28:21 -0500

    estructura del proyecto

--- src/services/userService.js
+++
@@ -1,9 +0,0 @@
-import apiClient from '../utils/apiClient';
-
-const getUsers = async (token) => {
-  const response = await apiClient(token).get(
-    'https://loginexpress-ts-jwt.onrender.com/api/auth/users'
-  );
-  return response.users;
-};
-export default getUsers;
